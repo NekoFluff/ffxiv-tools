01a94fd506a172bf9781bc994d887d19396e276c
Alex Nou <alexnou@gmail.com>
2024-02-06 22:46:19 -0700

    feat: print out execution time

--- app/Console/Commands/CalculateRecipeProfit.php
+++ app/Console/Commands/CalculateRecipeProfit.php
@@ -27,8 +27,8 @@ class CalculateRecipeProfit extends Command
      */
     public function handle()
     {
+        $startTime = microtime(true);
         $itemNames = ['Amdapori Wall Lantern', 'Large Woven Rug', 'Ahriman Vase', 'Nymian Wall Lantern', 'Drachen Armor Augmentation', 'Highland Barding', 'Large Garden Pond', 'Double Feather Bed', 'Levin Barding', "Champion's Lance", 'Taffeta Loincloth', 'Jade Crook', 'Woolen Gaskins', 'Sheep Rug', 'Oasis Pillar', 'Taffeta Shawl', 'Potted Dragon Tree', 'Riviera Wardrobe', 'Cobalt Haubergeon', 'Aeolian Scimitar', 'Kotatsu Table', 'Manor Sofa', 'Hardsilver Bangle of Casting', 'Manor Couch', 'Glade Wardrobe', 'Walnut Cartonnier', 'Ash Cabinet', 'Ice Barding', "Belah'dian Crystal Lantern", 'Tidal Barding', 'Wine Barrel', 'Titanium Mask of Striking', 'South Seas Couch', 'Tatami Mat', 'Masonwork Interior Wall', 'Cobalt Winglet', 'Mythril Ear Cuffs', 'Display Stand', 'Cotton Doublet Vest of Crafting', 'Tier 1 Aquarium', 'Marble Flooring', 'Manor Fireplace', 'Oasis Couch', 'Mythrite Bangle of Aiming', 'Holy Cedar Composite Bow', 'Bathroom Floor Tiles', "Initiate's Thighboots", 'Horn Staff', 'Easel', 'Glade Pillar', 'Glade Half Partition', 'Mythril Choker', 'Oasis Stall', 'Sanguine Scepter', 'Dhalmelskin Leggings of Scouting', 'Mythril Cuirass', 'Potted Spider Plant', 'Oak Composite Bow', "Madman's Whispering Rod", 'Ivy Pillar', 'Manor Candelabra', "Boarskin Smithy's Gloves", "Vamper's Knives", 'Boarskin Ringbands', "Erudite's Picatrix of Casting", 'Toadskin Jacket', 'Budding Rosewood Wand', 'Mythril Sollerets', 'Raptorskin Jerkin', 'Moonfire Sandals', "Chirurgeon's Curtain", 'Mahogany Partition Door', 'Oaken Bench', 'Cobalt Halberd', 'Mythrite Earrings of Gathering', 'Rainbow Ribbon of Fending', 'Wyvernskin Workboots', 'Yew Longbow', 'Corner Counter', "Barbarian's Bardiche", 'Brass Wristlets of Crafting', 'Archaeoskin Breeches of Crafting', 'Electrum Circlet (Spinel)', 'Planter Box', 'Wolf Earrings', 'Straight Stepping Stones', "Potted Oliphant's Ear", 'Tailor-made Eel Pie', 'Red Carpet', "Bridesmaid's Tights", 'Masonwork Stove', 'Mythril Earrings', 'Corner Hedge Partition', 'Company Chest', 'Mythrite Bangle of Fending', 'Fingerless Raptorskin Gloves', 'Linen Shirt', 'Mythril Wristlets of Crafting', 'Steel Frypan', 'Oak Longbow'];
-        // $itemNames = ['Amdapori Wall Lantern', 'Large Woven Rug'];
 
         $xivController = new XIVController();
         $universalisController = new UniversalisController();
@@ -61,7 +61,7 @@ class CalculateRecipeProfit extends Command
                 ];
             }
 
-            sleep(10);
+            sleep(1);
         }
 
         usort($results, function ($a, $b) {
@@ -74,5 +74,9 @@ class CalculateRecipeProfit extends Command
         $datetime = date('Y-m-d_H-i-s');
         $filename = 'recipe_profit_' . $datetime . '.json';
         file_put_contents($filename, json_encode($results));
+
+        $endTime = microtime(true);
+        $executionTime = $endTime - $startTime;
+        $this->info('Runtime: ' . $executionTime . ' seconds');
     }
 }
