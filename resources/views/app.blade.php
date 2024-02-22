4a9242439b987938b81cc7dd9dcff9f0144b0832
Alex Nou <alexnou@gmail.com>
2024-02-22 00:24:32 -0700

    fix: add pages to vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
-    @vite('resources/Pages/**/*.vue')
+    @vite('resources/js/Pages/**/*.vue')
 
     @inertiaHead
 </head>
