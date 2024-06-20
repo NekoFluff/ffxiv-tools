b367feb2abbc71bf07eca0c21244944b5b07c9c3
NekoFluff <alexnou@gmail.com>
2024-06-20 00:57:21 -0700

    chore: fix phpstan errors

--- app/Http/Requests/StoreRetainerRequest.php
+++ app/Http/Requests/StoreRetainerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\Enums\Server;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
