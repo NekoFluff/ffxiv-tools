b3b964570f346abad16394179a451ed75086e315
NekoFluff <alexnou@gmail.com>
2024-09-01 23:41:36 -0700

    chore: update debounce to 250ms

--- resources/views/livewire/item-search-bar.blade.php
+++ resources/views/livewire/item-search-bar.blade.php
@@ -1,7 +1,7 @@
 <div>
     <input
         class="w-full p-2 text-black bg-white border-gray-300 rounded-md shadow-md outline-none placeholder-slate-700 shadow-grey-900"
-        type="text" placeholder="Search..." wire:model.live.debounce.150ms="search" />
+        type="text" placeholder="Search..." wire:model.live.debounce.250ms="search" />
 
     <div class="relative z-10">
         <div class="absolute w-full overflow-auto bg-blue-500 max-h-96 scrollbar">
