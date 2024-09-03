fa54af771c882a5e85d7929953783ff7ca313d76
NekoFluff <alexnou@gmail.com>
2024-09-03 12:31:02 -0700

    chore: run in background

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,8 @@ use App\Console\Commands\RefreshOldItems;
 use App\Console\Commands\RefreshProfitableRecipes;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(RefreshOldItems::class)->cron('*/15 * * * *');
+Schedule::command(RefreshOldItems::class)->cron('*/15 * * * *')->runInBackground();
 
-Schedule::command(RefreshMostRecentlyUpdatedItems::class)->cron('*/3 * * * *');
+Schedule::command(RefreshMostRecentlyUpdatedItems::class)->cron('*/3 * * * *')->runInBackground();
 
-Schedule::command(RefreshProfitableRecipes::class)->cron('0 9 * * *');
+Schedule::command(RefreshProfitableRecipes::class)->cron('0 9 * * *')->runInBackground();
