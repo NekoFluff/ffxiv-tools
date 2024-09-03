63a7f91edbae8d3facaeec49b917c38bfa72e126
NekoFluff <alexnou@gmail.com>
2024-09-02 19:00:34 -0700

    feat: display retainer name on edit retainer page

--- resources/views/livewire/edit-retainer.blade.php
+++ resources/views/livewire/edit-retainer.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Retainer') }}
+            {{ $retainer->name }}
         </h2>
     </x-slot>
 
