7cd2c57608dbdcd384cf67b4835e8b8729ae67eb
NekoFluff <alexnou@gmail.com>
2024-09-10 17:57:21 -0700

    chore: prune telescope data older than 48 hours

--- routes/console.php
+++ routes/console.php
@@ -10,3 +10,5 @@ Schedule::command(RefreshOldItems::class)->cron('*/15 * * * *')->runInBackground
 Schedule::command(RefreshMostRecentlyUpdatedItems::class)->cron('* * * * *')->runInBackground();
 
 Schedule::command(RefreshProfitableRecipes::class)->cron('0 9 * * *')->runInBackground();
+
+Schedule::command('telescope:prune --hours=48')->daily();
