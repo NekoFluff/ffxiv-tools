a6f502bb4175d08d437be885554ad84a656de13e
NekoFluff <alexnou@gmail.com>
2024-10-05 14:57:54 -0700

    chore: update search bar appearance

--- resources/views/livewire/item-search-bar.blade.php
+++ resources/views/livewire/item-search-bar.blade.php
@@ -4,13 +4,13 @@
         type="text" placeholder="Search..." wire:model.live.debounce.250ms="search" />
 
     <div class="relative z-10">
-        <div class="absolute w-full overflow-auto bg-blue-500 max-h-96 scrollbar">
+        <div class="absolute w-full p-2 mt-2 overflow-auto bg-white rounded-sm max-h-96 scrollbar">
             @foreach ($items as $item)
                 <a wire:key="{{ $item->id }}"
-                    class="flex flex-row items-center w-full p-3 rounded-md hover:bg-blue-600"
+                    class="flex flex-row items-center w-full p-2 rounded-md hover:bg-zinc-100"
                     href="{{ route('item.show', $item->id) }}">
                     <img class="inline w-6 h-6" src="{{ 'https://xivapi.com/' . $item->icon }}" />
-                    <span class="ml-2 text-sm text-white">{{ $item->name }}</span>
+                    <span class="ml-2 text-sm text-black">{{ $item->name }}</span>
                 </a>
             @endforeach
         </div>
