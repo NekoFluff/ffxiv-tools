58e0473549be93b391030d69b4b6a9e89202c243
NekoFluff <alexnou@gmail.com>
2025-02-25 09:41:40 -0700

    chore: more logs

--- app/Livewire/ItemDashboard.php
+++ app/Livewire/ItemDashboard.php
@@ -31,7 +31,8 @@ class ItemDashboard extends Component
 
         $recalculate = boolval(request()->query('recalculate', '0'));
         Log::info('Recalculate: '.$recalculate);
-        Log::info('Diff in minutes: '.$this->item->marketPrice($this->server)->updated_at?->diffInMinutes(now()));
+        Log::info('Server'. $this->server);
+        Log::info('Diff in minutes: '.$this->item->marketPrice($this->server)?->updated_at?->diffInMinutes(now()));
         if ($recalculate || ! $this->item || $this->item->marketPrice($this->server) === null || $this->item->marketPrice($this->server)->updated_at?->diffInMinutes(now()) > 15) {
             Log::info('Dispatch: '.$this->item);
             RefreshItem::dispatch($id, $this->server);
